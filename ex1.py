c3ee6ea8116be7e70fea4b845a2be289cc61015f
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-01-11 15:02:23 +0530

    Added blank line at end of the file

--- ex1.py
+++ ex1.py
@@ -9,7 +9,7 @@ print 'I "said" do not touch this.'
 
 # Understands Following things in Exercise 1
 # How to create .py file.
-# How to execute first program like "Hello Word!!!".
+# How to execute first program like "Hello Word!!!". (python ex1.py)
 # How to run python file on terminal.
 # How errors will display.
 # Use '#' to commentout single line.
