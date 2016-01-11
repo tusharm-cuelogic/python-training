dc9898ca5aafb24953e7fdf7391c2711f3c8f5eb
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-01-11 18:43:08 +0530

    Completed Exercise 5

---
+++ ex5.py
@@ -0,0 +1,21 @@
+name = 'Tushar Mate'
+age = 28
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = "White"
+hair = 'Black'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall" % height
+print "He's %d pounds heavy" % weight
+print "Actully that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+print "If I add %d, %r and %d I get %r" % (age, height, weight, age + height + weight)
+
+
+# Understands following points in exercise 5
+# Make use of %d, %s and %r to display value of varibales in print statement.
+# %d for integer, %s for string and %r for both.
