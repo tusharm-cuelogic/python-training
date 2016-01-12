e7ceec08018251e8d2172356dc350fea1246fb9f
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-01-12 15:39:28 +0530

    Completed Exercise 11

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight)
+
+
+# Understands following points in exercise 11
+# Understood how to take input from user using rew_input() function.
