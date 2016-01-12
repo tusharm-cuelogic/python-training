d54e88090fc3fb5acdb0b1c049fdbaf571e00e88
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-01-12 15:16:34 +0530

    Completed Exercise 10

---
+++ ex10.py
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\n on a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishehs
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+# Understands following points in exercise 10
+# Make use of escape characters.
