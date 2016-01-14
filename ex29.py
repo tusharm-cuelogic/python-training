4f82720346bd42797aef5553d04be80209486fe9
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-01-14 14:53:27 +0530

    Completed Exercise 29

---
+++ ex29.py
@@ -0,0 +1,31 @@
+
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats! The word is doomed!"
+
+if people > cats:
+	print "Not Many cats! The world is saved!"
+
+if people < dogs:
+	print "The world is drooled on!"
+
+if people > dogs:
+	print "The world is dry!"
+
+dogs += 5
+
+if people >= dogs:
+	print "people are greater than or equal to dogs."
+
+if people <= dogs:
+	print "people are less than or equal to dogs."
+
+if people == dogs:
+	print "people are dogs."
+
+
+# Exercise 29
+# Understood if statement syntax and its use.
